cb690f9f69f9b7a4f60f2663cb35600d2c270751
shaked dollberg <shakedd@moveo.co.il>
2024-03-25 16:52:29 +0200

    changed steps to be array

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import {
@@ -13,6 +13,9 @@ import {
   FormField,
   FormGroupFields,
 } from '../../models/form-fields.model';
+import { CandidateQuestionnaireStepComponent } from '../steps/candidate-questionnaire-step/candidate-questionnaire-step.component';
+import { PersonalInformationStepComponent } from '../steps/personal-Information-step/personal-information-step.component';
+import { PreviousJobsStepComponent } from '../steps/previous-jobs-step/previous-jobs-step.component';
 
 @Component({
   selector: 'app-registration',
@@ -23,6 +26,8 @@ export class RegistrationComponent implements OnInit {
   registrationForm: FormGroup;
   userType: string;
   currentStep: number = 1;
+  FIRST_STEP: number = 1;
+  steps;
 
   candidateQuestionnaireFields: FormField[] = candidateQuestionnaireFields;
   personalInformationFields: (FormField | FormGroupFields)[] = personalInformationFields;
@@ -32,7 +37,7 @@ export class RegistrationComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -41,6 +46,11 @@ export class RegistrationComponent implements OnInit {
       if (type && Object.values(UserType).includes(type as UserType)) {
         this.userType = type;
         this.initializeForm();
+        this.steps = [
+          { component: CandidateQuestionnaireStepComponent, formGroup: {'formGroup' : this.registrationForm.get('stepOne')} },
+          { component: PersonalInformationStepComponent, formGroup: {'formGroup' : this.registrationForm.get('stepTwo')} },
+          { component: PreviousJobsStepComponent, formGroup: {'formGroup' : this.registrationForm.get('stepThree')} }
+        ];
       } else {
         this.router.navigate(['/registration/general']);
       }
